a4fcca54c039e61933050fbd5886bfd9e78fbe0c
Madhav Asok <masok@echo360.com>
2023-04-04 13:08:02 -0700

    Initial upload

---
+++ gui/Components/Buttons/BigThreeButtons.java
@@ -0,0 +1,12 @@
+package Components.Buttons;
+
+import javax.swing.JButton;
+import java.awt.Color;
+
+public class BigThreeButtons {
+
+    public BigThreeButtons(String name, Color color){
+        JButton button = new JButton(name);
+        button.setBackground(color);
+    }
+}
---
+++ gui/Components/Frame.java
@@ -0,0 +1,13 @@
+package Components;
+import javax.swing.*;
+
+public class Frame extends JFrame{
+
+    public Frame(){
+        JFrame frame = new JFrame("MainFrame");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1000, 600);
+
+    }
+
+}
