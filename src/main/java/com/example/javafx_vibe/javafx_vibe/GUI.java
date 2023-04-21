e2ecf6dffc3f9a8d49c020cc9fc2c5520e8450ec
MaxLewter16 <lewtermax@gmail.com>
2023-04-21 10:08:24 -0700

    initial commit

---
+++ src/main/java/com/example/javafx_vibe/javafx_vibe/GUI.java
@@ -0,0 +1,15 @@
+package com.example.javafx_vibe.javafx_vibe;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
