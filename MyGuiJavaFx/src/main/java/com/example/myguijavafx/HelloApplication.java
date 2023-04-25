80c054f12ed87be0ffabfbbd54a68d072f3a0bce
Madhav Asok <masok@echo360.com>
2023-04-24 17:05:53 -0700

    Added Jonathan's code

--- MyGuiJavaFx/src/main/java/com/example/myguijavafx/HelloApplication.java
+++ MyGuiJavaFx/src/main/java/com/example/myguijavafx/HelloApplication.java
@@ -3,65 +3,25 @@ package com.example.myguijavafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import jssc.SerialPort;
-import jssc.SerialPortEvent;
-import jssc.SerialPortEventListener;
-import jssc.SerialPortException;
+
 import java.io.IOException;
+import com.fazecast.jSerialComm.SerialPort;
 
-public class HelloApplication extends Application
-{
 
-    private SerialPort serialPort;
+public class HelloApplication extends Application {
 
     @Override
-    public void start(Stage stage) throws Exception
-    {
-        VBox root = new VBox();
-        Button button = new Button("Send data to Arduino");
-        button.setOnAction(event -> {
-            try {
-                serialPort.writeString("Hello, Arduino!");
-            } catch (SerialPortException e) {
-                e.printStackTrace();
-            }
-        });
-        root.getChildren().add(button);
+    public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 640, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
 
-        serialPort = new SerialPort("/dev/ttyACM0"); // Replace with the correct port name for your system
-        serialPort.openPort();
-        serialPort.setParams(9600, 8, 1, 0);
-        serialPort.addEventListener(new SerialPortEventListener() {
-            @Override
-            public void serialEvent(SerialPortEvent serialPortEvent) {
-                if (serialPortEvent.isRXCHAR()) {
-                    try {
-                        String data = serialPort.readString();
-                        // Process the received data from Arduino
-                    } catch (SerialPortException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
     }
 
-    @Override
-    public void stop() throws Exception {
-        // Close the serial port when the JavaFX application stops
-        serialPort.closePort();
-        super.stop();
-    }
-    public static void main(String[] args)
-    {
-        launch(args);
+    public static void main(String[] args) {
+        launch();
     }
 }
--- MyGuiJavaFx/src/main/java/com/example/myguijavafx/HelloController.java
+++ MyGuiJavaFx/src/main/java/com/example/myguijavafx/HelloController.java
@@ -2,15 +2,22 @@ package com.example.myguijavafx;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import com.fazecast.jSerialComm.*;
 
-public class HelloController
-{
+public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick()
-    {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void onSendButtonClick() {
+        //int i = 1;
+        SerialPort comPort = SerialPort.getCommPort("/dev/cu.usbserial-1130");
+        comPort.setComPortParameters(9600, 8, 1, SerialPort.NO_PARITY);
+        comPort.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING,0,0);
+
+        comPort.openPort();
+        comPort.writeBytes("h".getBytes(), "h".length());
+        comPort.closePort();
     }
+
 }
