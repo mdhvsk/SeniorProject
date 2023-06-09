875c03ca4edaeceb3e357e4c4c3fe35a70dd7681
MaxLewter16 <lewtermax@gmail.com>
2023-06-08 17:33:29 -0700

    working version, needs to be cleaned

--- src/main/java/com/example/javafx_vibe/javafx_vibe/MainController.java
+++ src/main/java/com/example/javafx_vibe/javafx_vibe/MainController.java
@@ -44,7 +44,7 @@ public class MainController implements Initializable
     private XYChart.Series<Number, Number> zSeries = new XYChart.Series<>();
     @FXML
     private MenuItem Exit;
-    private SerialPort comPort;
+    private static SerialPort comPort;
 
     @FXML
     private Button btnStart;
@@ -69,7 +69,25 @@ public class MainController implements Initializable
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1){
-        setComPort(SerialPort.getCommPort("/dev/tty.usbmodem11301"));
+        SerialPort[] portList = SerialPort.getCommPorts();
+        System.out.print(portList[0].getPortDescription());
+        String arduinoDescription = "Arduino";
+
+        for (SerialPort port : portList) {
+
+            System.out.println(port.getSystemPortName() + ": " + port.getDescriptivePortName());
+            String portDescription = port.getPortDescription();
+            if (portDescription.contains(arduinoDescription)) {
+                // Try to open the port
+                try{port.openPort();
+                    System.out.println("Connected to Arduino on port: " + port.getSystemPortName());
+                    setComPort(port);
+                    // You can perform further operations with the opened port here
+                } catch(Exception e) {
+                    System.out.println("Failed to open port: " + port.getSystemPortName());
+                }
+            }
+        }
         SpinnerValueFactory<Integer> timeValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 240);
         timeValueFactory.setValue(0);
         time.setValueFactory(timeValueFactory);
@@ -102,8 +120,7 @@ public class MainController implements Initializable
     }
 
     @FXML
-    void handle_btnStop(ActionEvent event) throws IOException
-    {
+    void handle_btnStop(ActionEvent event) throws IOException {
         System.out.println("\nStop button clicked");
         stopFlag = true;
 
@@ -112,48 +129,6 @@ public class MainController implements Initializable
 //        String output = "s:3:1";
         outputStream1.write(customOutput.getBytes());
         outputStream1.flush();
-
-        SerialPort comPort = ArduinoUtils.findArduinoPort();
-        setComPort(comPort);
-
-        String filePath = "accelerometer_data.csv";
-        CSVWriter csvWriter = new CSVWriter(new FileWriter(filePath));
-
-        long startTime = System.currentTimeMillis();
-
-        Thread dataThread = new Thread(() -> {
-            try {
-                InputStream inputStream = comPort.getInputStream();
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-                while (!stopFlag) {
-                    // Process the line of data (e.g., split it into x, y, z values)
-                    var line = bufferedReader.readLine();
-                    System.out.println(line);
-                    long currentTime = System.currentTimeMillis();
-                    double elapsedTime = (double) (currentTime - startTime) / 1000;
-                    try {
-                        var accelerationData = AccelerationData.from_arduino(line, elapsedTime);
-                        csvWriter.writeNext(accelerationData.toCsvStrings());
-                    } catch (Exception e) {
-                        System.out.println("Failed to parse arduino data: " + e);
-                    }
-                }
-                inputStream.close();
-                inputStreamReader.close();
-                csvWriter.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        dataThread.start();
-    }
-
-    @FXML
-    void handle_btnStop(ActionEvent event) throws IOException {
-        System.out.println("\nStop button clicked");
-        stopFlag = true;
-        comPort.closePort();
     }
 
 
