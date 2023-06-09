e85333ef8830a739a233c23ace05ae990d736495
MaxLewter16 <lewtermax@gmail.com>
2023-06-08 17:35:06 -0700

    working version, needs to be cleaned

--- arduino_code/customMotorRampUp/customMotorRampUp.ino
+++ arduino_code/customMotorRampUp/customMotorRampUp.ino
@@ -37,7 +37,7 @@ void generatePWM(unsigned long onTime, unsigned long offTime, int duty ){
   //   digitalWrite(53,HIGH);
   //   digitalWrite(52,LOW);
   // }
-  
+
 // 10 second ramp up run
   if (elapsedMillis <= onTime && elapsedMillis >= 10000 && onTime - elapsedMillis <= 10000) {
     // Set PWM signal ON with specified duty cycle
@@ -51,7 +51,7 @@ void generatePWM(unsigned long onTime, unsigned long offTime, int duty ){
   //   digitalWrite(53,HIGH);
   //   digitalWrite(52,LOW);
   // }
- 
+
   //   if(onTime - elapsedMillis < 10000){
   //   analogWrite(pwmPin, ((onTime-elapsedMillis)/10000) * duty * 255 / 100);
   //   digitalWrite(53,HIGH);
@@ -166,7 +166,7 @@ void loop () {
       digitalWrite(52,HIGH);
       return;
     }
-    
+
     intensityIndex++; // changes intensity 
   // Turn on motor if input given or if new cycle starts
      motorRunning = true; //continues generatePWM
