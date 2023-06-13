1b2123e79a683ba37881f6c26f4bd25582c2e990
Madhav Asok <masok@echo360.com>
2023-06-13 12:14:15 -0700

    Clean up unused files and comments

--- MyGuiJavaFx/src/main/java/com/example/myguijavafx/Gui.java
+++
@@ -1,6 +0,0 @@
-package com.example.myguijavafx;
-
-public class Gui {
-
-}
-
--- src/main/java/com/example/javafx_vibe/javafx_vibe/MainController.java
+++ src/main/java/com/example/javafx_vibe/javafx_vibe/MainController.java
@@ -45,27 +45,18 @@ public class MainController implements Initializable
     @FXML
     private MenuItem Exit;
     private static SerialPort comPort;
-
     @FXML
     private Button btnStart;
-
     @FXML
     private Button btnStop;
-
     @FXML
     private Button btnChart;
-
-
     @FXML
     private Spinner<Integer> time;
     @FXML
     private Spinner<Integer> intensity;
     private int currentTimeValue;
     private int currentIntensityValue;
-
-//    Mac version
-//    protected static SerialPort macArduinoPort = SerialPort.getCommPort("/dev/tty.usbserial-1130");
-//    protected static SerialPort macArduinoPort = SerialPort.getCommPort("/dev/tty.usbmodem11301");
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1){
@@ -99,18 +90,6 @@ public class MainController implements Initializable
         comPort.setComPortParameters(9600, 8, 1, SerialPort.NO_PARITY);
         comPort.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0,0 );
         comPort.openPort();
-//        setComPort(SerialPort.getCommPort("/dev/tty.usbmodem11301"));
-//        SpinnerValueFactory<Integer> timeValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 120);
-//        timeValueFactory.setValue(0);
-//        time.setValueFactory(timeValueFactory);
-//        time.valueProperty().addListener((observableValue, integer, t1) -> setCurrentTimeValue(time.getValue()));
-//        SpinnerValueFactory<Integer> intensityValueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(1, 100);
-//        timeValueFactory.setValue(0);
-//        intensity.setValueFactory(intensityValueFactory);
-//        intensity.valueProperty().addListener((observableValue, integer, t1) -> setCurrentIntensityValue(intensity.getValue()));
-//        comPort.setComPortParameters(9600, 8, 1, SerialPort.NO_PARITY);
-//        comPort.setComPortTimeouts(SerialPort.TIMEOUT_WRITE_BLOCKING, 0,0 );
-//        comPort.openPort();
     }
 
 
@@ -123,39 +102,12 @@ public class MainController implements Initializable
     void handle_btnStop(ActionEvent event) throws IOException {
         System.out.println("\nStop button clicked");
         stopFlag = true;
-
         OutputStream outputStream1 = comPort.getOutputStream();
         String customOutput = "t";
-//        String output = "s:3:1";
         outputStream1.write(customOutput.getBytes());
         outputStream1.flush();
     }
-
-
-    @FXML
-    void handle_btnChart(ActionEvent event) {
-        try {
-//            FXMLLoader loader = new FXMLLoader(getClass().getResource("file:///C:/Users/Owner/Code/javafx_vibe/src/main/resources/com/example/javafx_vibe/chart.fxml"));
-            FXMLLoader loader = new FXMLLoader();
-            URL url = new File("src/main/resources/com/example/javafx_vibe/charts.fxml").toURI().toURL();
-            loader.setLocation(url);
-            AnchorPane pane = loader.load();
-
-            DataController dataController = loader.getController();
-            dataController.parseData("accelerometer_data.csv");
-
-            Stage stage = new Stage();
-            stage.setScene(new Scene(pane));
-            stage.setTitle("Data Visualization");
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (CsvValidationException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
+    
     @FXML
     void handleMotorStart() throws IOException
     {
@@ -165,22 +117,11 @@ public class MainController implements Initializable
         System.out.println(intensityStr);
         OutputStream outputStream1 = comPort.getOutputStream();
         String customOutput = "\"s:" + timeStr + ":" + intensityStr + "\"";
-//        String output = "s:3:1";
         System.out.println(customOutput);
         outputStream1.write(customOutput.getBytes());
         outputStream1.flush();
     }
 
-
-    @FXML
-    void handle_menuCustom(ActionEvent event) {
-
-    }
-
-    @FXML
-    void handle_menuPreset1(ActionEvent event) {
-
-    }
 
     void setComPort(SerialPort comPort){
         this.comPort = comPort;
